bba1487560932906a0d6bc8e52a8f25ce0d4f129
fomativeh <sim21091994@gmail.com>
2026-01-15 11:15:18 +0100

    .

--- helpers/launchbot.js
+++ helpers/launchbot.js
@@ -3,24 +3,7 @@ const launchBot = (bot) => {
     .getMe()
     .then((botInfo) => {
       console.log(`Bot ${botInfo.username} is connected and running.`);
-      bot.launch({
-        allowedUpdates: [
-          "message",
-          "edited_message",
-          "channel_post",
-          "edited_channel_post",
-          "inline_query",
-          "chosen_inline_result",
-          "callback_query",
-          "shipping_query",
-          "pre_checkout_query",
-          "poll",
-          "poll_answer",
-          "my_chat_member",
-          "chat_member",
-          "chat_join_request",
-        ],
-      });
+      bot.launch()
     })
     .catch((err) => {
       console.error("Error connecting bot:", err);
